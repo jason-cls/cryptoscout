b414461554137a37074dc831b4204a92c700fe47
Jason Chong <jason.chongleeshin@gmail.com>
2022-10-09 17:07:13 -0400

    add local provider for sa keygen

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,10 +1,11 @@
 locals {
-  credentials = "${path.root}/secrets/terraform-sa-key.json"
-  project     = "cryptoscout"
-  region      = "us-central1"
-  zone        = "us-central1-b"
+  project = "cryptoscout"
+  region  = "us-central1"
+  zone    = "us-central1-b"
 
   terraform_sa_name = "terraform"
+  secrets_dir       = "${path.root}/secrets"
+  credentials       = "${local.secrets_dir}/terraform-sa-key.json"
 }
 
 terraform {
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 4.39.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2.3"
+    }
   }
 }
 
@@ -19,6 +23,16 @@ resource "google_service_account" "batch" {
   account_id   = var.batch_service_account_id
   display_name = "Batch Ingestor"
   description  = "Executes batch ingestion jobs"
+}
+
+resource "google_service_account_key" "batchkey" {
+  service_account_id = google_service_account.batch.id
+}
+
+resource "local_sensitive_file" "batch_service_account_key" {
+  filename        = "${local.secrets_dir}/batch-sa-key.json"
+  content         = base64decode(google_service_account_key.batchkey.private_key)
+  file_permission = "0444"
 }
 
 resource "google_project_iam_member" "batch" {
