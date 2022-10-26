c09a4acdfff0ba99c8b50caabfa684b20135c58a
Jason Chong <jason.chongleeshin@gmail.com>
2022-10-25 23:40:48 -0400

    Add app API routes

---
+++ batch_ingest/src/batch_ingest/main.py
@@ -0,0 +1,178 @@
+import configparser
+import inspect
+import json
+import logging
+import logging.config
+import os
+import time
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from pipelines.coincap_to_gcs import (
+    ingest_asset_history,
+    ingest_asset_info,
+    ingest_exchange_info,
+    ingest_market_history,
+)
+
+# Setup
+file_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(file_dir)
+logging.config.fileConfig("logging.cfg", disable_existing_loggers=False)
+logging.Formatter.converter = time.gmtime
+
+# Environment vars
+load_dotenv(override=False)
+COINCAP_API_KEY = os.environ["COINCAP_API_KEY"]
+
+# Configuration
+config = configparser.ConfigParser()
+config.read("pipeline.cfg")
+coincap_cfg = config["coincap.api"]
+INTERVAL = coincap_cfg["historical_data_interval"]
+QUOTE_ID = coincap_cfg["market_history_quote_id"]
+ASSET_IDS = json.loads(coincap_cfg["asset_ids"])
+EXCHANGE_IDS = json.loads(coincap_cfg["exchange_ids"])
+TARGET_BUCKET = config["gcs"]["target_bucket"]
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """Returns a list of exposed API endpoints and their respective HTTP methods."""
+    return {
+        "endpoints": {
+            route.path: route.methods
+            for route in app.routes
+            if isinstance(route, APIRoute)
+        }
+    }
+
+
+@app.get("/ingestAssetInfo")
+def ingest_asset_info_job():
+    """Ingests current asset info snapshot data for a fixed set of assets."""
+    success = True
+    for asset_id in ASSET_IDS:
+        success *= ingest_asset_info(asset_id, COINCAP_API_KEY, TARGET_BUCKET)
+    if success:
+        msg = "Successfully ingested all asset info data!"
+        logging.info(msg)
+    else:
+        msg = "Failed to ingest all asset info data."
+        logging.error(msg)
+    locvars = locals()
+    return {
+        "success": success,
+        "message": msg,
+        "endpoint": "/ingestAssetInfo",
+        "request_params": {
+            k: locvars.get(k)
+            for k in inspect.signature(ingest_asset_info_job).parameters.keys()
+        },
+        "timestamp": time.time(),
+    }
+
+
+@app.get("/ingestAssetHistory")
+def ingest_asset_history_job(unix_start_ms: int, unix_end_ms: int):
+    """Ingests historical asset data within a time interval for a fixed set of assets.
+    """
+    success = True
+    for asset_id in ASSET_IDS:
+        success *= ingest_asset_history(
+            asset_id,
+            COINCAP_API_KEY,
+            unix_start_ms,
+            unix_end_ms,
+            INTERVAL,
+            TARGET_BUCKET,
+        )
+    if success:
+        msg = "Successfully ingested all asset history data!"
+        logging.info(msg)
+    else:
+        msg = "Failed to ingest all asset history data."
+        logging.error(msg)
+    locvars = locals()
+    return {
+        "success": success,
+        "message": msg,
+        "endpoint": "/ingestAssetHistory",
+        "request_params": {
+            k: locvars.get(k)
+            for k in inspect.signature(ingest_asset_history_job).parameters.keys()
+        },
+        "timestamp": time.time(),
+    }
+
+
+@app.get("/ingestExchangeInfo")
+def ingest_exchange_info_job():
+    """Ingests current exchange info snapshot data for a fixed set of exchanges."""
+    success = True
+    for exchange_id in EXCHANGE_IDS:
+        success *= ingest_exchange_info(exchange_id, COINCAP_API_KEY, TARGET_BUCKET)
+    if success:
+        msg = "Successfully ingested all exchange info data!"
+        logging.info(msg)
+    else:
+        msg = "Failed to ingest all exchange info data."
+        logging.error(msg)
+    locvars = locals()
+    return {
+        "success": success,
+        "message": msg,
+        "endpoint": "/ingestExchangeInfo",
+        "request_params": {
+            k: locvars.get(k)
+            for k in inspect.signature(ingest_exchange_info_job).parameters.keys()
+        },
+        "timestamp": time.time(),
+    }
+
+
+@app.get("/ingestMarketHistory")
+def ingest_market_history_job(unix_start_ms: int, unix_end_ms: int):
+    """Ingests historical market data (OHLCV candles) within a time interval for a
+    fixed combination of assets and exchanges.
+    """
+    success = True
+    for exchange_id in EXCHANGE_IDS:
+        for asset_id in ASSET_IDS:
+            success *= ingest_market_history(
+                exchange_id,
+                asset_id,
+                QUOTE_ID,
+                COINCAP_API_KEY,
+                unix_start_ms,
+                unix_end_ms,
+                INTERVAL,
+                TARGET_BUCKET,
+            )
+    if success:
+        msg = "Successfully ingested all market history data!"
+        logging.info(msg)
+    else:
+        msg = "Failed to ingest all market history data."
+        logging.error(msg)
+    locvars = locals()
+    return {
+        "success": success,
+        "message": msg,
+        "endpoint": "/ingestMarketHistory",
+        "request_params": {
+            k: locvars.get(k)
+            for k in inspect.signature(ingest_market_history_job).parameters.keys()
+        },
+        "timestamp": time.time(),
+    }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    server_port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=server_port, log_level="info")
