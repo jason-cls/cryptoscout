fc8a1c6fa680463beceeb09f95f0cb643a93c73b
Jason Chong <jason.chongleeshin@gmail.com>
2023-03-05 01:38:36 -0500

    Setup Airflow installation for dev and docker compose

---
+++ airflow/install/pip-airflow-constrain.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Installs Airflow locally using known-to-be-working constraints.
+
+set -eou pipefail
+
+AIRFLOW_VERSION=2.5.1
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+
+python -m pip install "apache-airflow[google]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+echo "Done installing Airflow ${AIRFLOW_VERSION} for Python ${PYTHON_VERSION} via pip"
