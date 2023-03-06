9f9285b3206bd8604d7fb98dfc1d2e582ea2a347
Jason Chong <jason.chongleeshin@gmail.com>
2023-03-05 23:56:46 -0500

    Add celery to dev requirements

--- airflow/install/pip-airflow-constrain.sh
+++ airflow/install/pip-airflow-constrain.sh
@@ -8,5 +8,5 @@ AIRFLOW_VERSION=2.5.1
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
-python -m pip install "apache-airflow[google]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+python -m pip install "apache-airflow[google,celery]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 echo "Done installing Airflow ${AIRFLOW_VERSION} for Python ${PYTHON_VERSION} via pip"
